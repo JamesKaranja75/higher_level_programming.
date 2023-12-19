9985dcb140868852465352b5f07a729959805972
JamesKaranja75 <karanjajames101@gmail.com>
2023-12-19 06:35:24 -0800

    A Python class MagicClass

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -7,9 +7,9 @@ class MagicClass:
     """Represent a circle."""
 
     def __init__(self, radius=0):
-        """initialize a MagicClass.
+        """Initialize a MagicClass.
 
-        Arg:
+        Args:
            radius (float or int): The radius of the new MagicClass.
         """
         self.__radius = 0
@@ -19,8 +19,8 @@ class MagicClass:
 
     def area(self):
         """Return the area of the MagicClass."""
-        return (self.__radius ** 2 = math.pi)
+        return (self.__radius ** 2) * math.pi
 
     def circumference(self):
         """Return the circumference of the MagicClass."""
-        return (2 * math.pi * self.__radius)
+        return 2 * math.pi * self.__radius
