23ecefe296d8f62696d34f4316d87735da1eae2e
JamesKaranja75 <karanjajames101@gmail.com>
2023-12-19 02:46:55 -0800

    A Python class MagicClass

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -10,7 +10,7 @@ class MagicClass:
         """initialize a MagicClass.
 
         Arg:
-        radius (float or int): The radius of the new MagicClass.
+           radius (float or int): The radius of the new MagicClass.
         """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
@@ -20,6 +20,7 @@ class MagicClass:
     def area(self):
         """Return the area of the MagicClass."""
         return (self.__radius ** 2 = math.pi)
+
     def circumference(self):
         """Return the circumference of the MagicClass."""
         return (2 * math.pi * self.__radius)
